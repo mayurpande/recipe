b949288d2fd7c7e864afac3ba4ac9b97637f15dc
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-13 15:52:00 +0100

    Commented out source property call as  out source property as its access modifier has been changed to private

--- classes/cookbook.php
+++ classes/cookbook.php
@@ -3,13 +3,13 @@ include "classes/recipe.php";
 //after creating the class a new object can be instansiated and stored in a variable
 $recipe1 = new Recipe();
 //access properties with object created
-$recipe1->source = "Grandma Holligan";
+//$recipe1->source = "Grandma Holligan";
 $recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg",1);
 $recipe1->addIngredient("flour",2,"cup");
 //new object of class
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+//$recipe2->source = "Betty Crocker";
 $recipe2->setTitle("My Second recipe");
 
 //now to use displayRecipe method we call it just like a regular fn
