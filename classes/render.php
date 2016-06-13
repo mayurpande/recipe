961dc1bdc880e28dc08e75f7745a1099c73135ba
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-13 17:03:19 +0100

    Modified output string to be formatted neatly

--- classes/render.php
+++ classes/render.php
@@ -19,7 +19,21 @@ class Render
 
 		//as we made title private, it is no longer in the same class so not accesible.
 		//so we need to specify the getTitle method here
-		return $recipe->getTitle() . " by " . $recipe->getSource();
+		$output = "";
+
+		$output .=  $recipe->getTitle() . " by " . $recipe->getSource();
+		$output .= "\n";
+		$output .= implode(", ",$recipe->getTags());
+		$output .= "\n\n";
+		foreach($recipe->getIngredients() as $ing){
+			$output .= $ing['amount'] . " " . $ing['measure'] . " " . $ing['item'];
+			$output .= "\n";
+		}
+		$output .= "\n";
+		$output .= implode("\n",$recipe->getInstructions());
+		$output .= "\n";
+		$output .= $recipe->getYield();
+		return $output;
 	}
 
 }
