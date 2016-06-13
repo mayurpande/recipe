dd5f1e06e4f3b3b045f0765e0a36d20aae4a0d0a
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-13 12:57:55 +0100

    Accessed displayMethod from outside the class

--- classes/recipe.php
+++ classes/recipe.php
@@ -28,13 +28,16 @@ class Recipe
 
 //after creating the class a new object can be instansiated and stored in a variable
 $recipe1 = new Recipe();
-
 //access properties with object created
-echo $recipe1->source;
 $recipe1->source = "Grandma Holligan";
-echo $recipe1->source;
+$recipe1->title = "My first recipe";
 
+//new object of class
 $recipe2 = new Recipe();
 $recipe2->source = "Betty Crocker";
-echo $recipe1->source;
-echo $recipe2->source;
+$recipe2->title = "My Second recipe";
+
+//now to use displayRecipe method we call it just like a regular fn
+//only difference is that we first need to reference the object that it belongs to
+echo $recipe1->displayRecipe();
+echo $recipe2->displayRecipe();
