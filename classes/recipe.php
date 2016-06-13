65a9bd5959f2c1997cbf7131cdff71338c7bcb6d
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-13 12:49:36 +0100

    Added method for diplaying recipe made use of  keyword to access methods own properties

--- classes/recipe.php
+++ classes/recipe.php
@@ -10,6 +10,20 @@ class Recipe
 	public $source = "Alena Holligan";
 	
 
+	public function displayRecipe()
+	{
+		/*within the method we describe the action we want to perform
+		 * for our methods to be really useful. we need them to have access
+		 * to the other methods and properties of their respective objects
+		 * php allows objects to reference themselves, using the keyword variable
+		 * this.
+		 * when working within the scope of a method use $this in the same way you 
+		 * would use the object name outside the class.
+		 * The $this keyword, indicates that we want to use the object's own properties or
+		 * methods and allows us to have access to them, in the class scope
+		 */
+		return $this->title . " by " . $this->source;
+	}
 }
 
 //after creating the class a new object can be instansiated and stored in a variable
