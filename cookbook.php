da02c3c6aab6b1de9d8cb04ad76073820ef6b02e
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-14 10:55:03 +0100

    Moved instansiating code file out of classes folder

---
+++ cookbook.php
@@ -0,0 +1,33 @@
+<?php
+include "classes/recipe.php";
+include "classes/render.php";
+
+//after creating the class a new object can be instansiated and stored in a variable
+$recipe1 = new Recipe();
+//access properties with object created
+$recipe1->setSource("Grandma Holligan");
+$recipe1->setTitle("my first recipe");
+$recipe1->addIngredient("egg",1);
+$recipe1->addIngredient("flour",2,"cup");
+//new object of class
+$recipe2 = new Recipe();
+$recipe2->setSource("Betty Crocker");
+$recipe2->setTitle("My Second recipe");
+
+//now to use displayRecipe method we call it just like a regular fn
+//only difference is that we first need to reference the object that it belongs to
+$recipe1->addInstruction("This is my first instruction");
+$recipe1->addInstruction("This is my second instruction");
+
+$recipe1->addTag("Breakfast");
+$recipe1->addTag("Main Course");
+
+$recipe1->setYield("6 servings");
+
+/*
+ * Now we are ready to call our static method
+ * To use a static method, we specify the class, then double colons
+ * and then the method
+ */
+
+echo Render::displayRecipe($recipe1);
