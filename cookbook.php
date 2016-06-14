3db6347f91c30dc467046f10c9ac6621545e2f22
Mayur Pande <mayurpande.uk@gmail.com>
2016-06-14 11:13:44 +0100

    We pass the title directly to the call fornew recipe

--- cookbook.php
+++ cookbook.php
@@ -3,16 +3,16 @@ include "classes/recipe.php";
 include "classes/render.php";
 
 //after creating the class a new object can be instansiated and stored in a variable
-$recipe1 = new Recipe();
+$recipe1 = new Recipe("my first recipe");
 //access properties with object created
 $recipe1->setSource("Grandma Holligan");
-$recipe1->setTitle("my first recipe");
+
 $recipe1->addIngredient("egg",1);
 $recipe1->addIngredient("flour",2,"cup");
 //new object of class
-$recipe2 = new Recipe();
+$recipe2 = new Recipe("my second recipe");
 $recipe2->setSource("Betty Crocker");
-$recipe2->setTitle("My Second recipe");
+
 
 //now to use displayRecipe method we call it just like a regular fn
 //only difference is that we first need to reference the object that it belongs to
